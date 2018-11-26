91901d2df410b26fcb41d7878b6d04676ffc9358
Luis Yama <jluis.yama@gmail.com>
2018-11-26 14:09:12 -0600

    Solving Problems

---
+++ app/config/configdb.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'DB_TYPE'    => 'mysql',
+    'DB_HOST'    => 'localhost',
+    'DB_NAME'    => 'control',
+    'DB_USER'    => 'root',
+    'DB_PASS'    => 'admin',
+    'DB_PORT'    => '3306',
+    'DB_CHARSET' => 'utf8',
+);
